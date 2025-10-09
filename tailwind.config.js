ef8d78908533f01e1ab56d2e70efbdc2571fc188
Prathameshnium <117578336+prathameshnium@users.noreply.github.com>
2025-10-10 04:17:00 +0530

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  content: [
+    './*.html',
+    './pages/**/*.html',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        serif: ['Source Serif Pro', 'serif'],
+      },
+      colors: {
+        'accent-orange': '#f6ad55', 
+        'dark-bg': '#1a202c',       
+        'card-bg': '#2d3748',
+        'light-slate': '#a8b2d1',
+        'slate': '#8892b0'
+      }
+    },
   },
   plugins: [],
 }
